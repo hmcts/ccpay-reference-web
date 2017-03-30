466ef1fdf32745533614bd71c3ccb40dfcaa6561
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-03-30 15:39:03 +0100

    Handling of invalid token

--- app/idam/routeAuthorisation.js
+++ app/idam/routeAuthorisation.js
@@ -3,7 +3,7 @@ const isUnprotectedPath = require('./routeHelper').isUnprotectedPath
 const userDetailsHolder = require('../../lib/auth/userDetailsHolder')
 
 module.exports = function (req, res, next) {
-  userDetailsHolder(req)
+  userDetailsHolder(req, res)
     .retrieve()
     .then(userDetails => {
       res.locals.isLoggedIn = userDetails !== null
